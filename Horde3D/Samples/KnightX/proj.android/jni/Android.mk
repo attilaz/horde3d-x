53c43e2c922aad36bfb6e537774ee17d2d903a84
attilaz <kocsisa78@gmail.com>
2013-08-16 15:39:41 +0200

    directory rename in vs2012 and android.mk projects: cocos2d-x-mini -> cocos2dx-mini

--- Horde3D/Samples/KnightX/proj.android/jni/Android.mk
+++ Horde3D/Samples/KnightX/proj.android/jni/Android.mk
@@ -19,7 +19,7 @@ LOCAL_WHOLE_STATIC_LIBRARIES += horde3d_utils_static
 
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,Horde3D/Samples/cocos2d-x-mini/proj.android)
+$(call import-module,Horde3D/Samples/cocos2dx-mini/proj.android)
 $(call import-module,Horde3D/Source/Horde3DEngine/proj.android)
 $(call import-module,Extensions/Terrain/Source/proj.android)
 $(call import-module,Horde3D/Source/Horde3DUtils/proj.android)
